5a2ee8969fa5aa0105cb9ed6364ac35b81f8d19b
Maximillian Vincent <maximillianvincent@maximilliansmbp.myfiosgateway.com>
2022-01-06 13:00:20 -0500

    Wrote two new functions in js app.

--- static/js/app.js
+++ static/js/app.js
@@ -60,14 +60,11 @@ function updateFilters() {
     // Set the filtered data to the tableData.
     let filteredData = tableData;
   
-    // 9. Loop through all of the filters and keep any data that
+    // Loop through all of the filters and keep any data that
     // matches the filter values
-    for (var i = 0; i < filters.length; i++) {
-      
-    }
-
-    
-    
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
   
     // Finally, rebuild the table using the filtered data
     buildTable(filteredData);
